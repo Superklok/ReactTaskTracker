18eac85e06cc8dd9b9c27179c0aea6586797706d
Trev Morin <trev@superklok.com>
2021-11-26 16:26:30 -0600

    Add Button Component

--- v1.x.x/src/components/Header.js
+++ v1.x.x/src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({title}) => {
+	const onClick = (e) => {
+		console.log('Click', e);
+	}
+
 	return (
-		<header>
+		<header className='header'>
 			<h1>{title}</h1>
+			<Button color='#F84AA7' text='Add' onClick={onClick} />
 		</header>
 	)
 }
