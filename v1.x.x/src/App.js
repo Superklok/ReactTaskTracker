f2f123deb51556cb6de81174e59ee4c1510587d0
Trev Morin <trev@superklok.com>
2021-11-26 18:47:41 -0600

    Add Task Destroy Function

--- v1.x.x/src/App.js
+++ v1.x.x/src/App.js
@@ -23,11 +23,17 @@ const App = () => {
 			reminder: false,
 		},
 	]);
+	
+	// Task destruction
+	const destroyTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+	}
 
 	return (
 		<div className="container">
 			<Header />
-			<Tasks tasks={tasks} />
+			<Tasks tasks={tasks}
+			onDestroy={destroyTask} />
 		</div>
 	);
 }
--- v1.x.x/src/components/Task.js
+++ v1.x.x/src/components/Task.js
@@ -1,10 +1,12 @@
 import {FaTimes} from 'react-icons/fa';
 
-const Task = ({task}) => {
+const Task = ({task, onDestroy}) => {
 	return (
 		<div className='task'>
 			<h3>
-				{task.text} <FaTimes style={{color: '#FF3562', cursor: 'pointer'}} />
+				{task.text} <FaTimes 
+				style={{color: '#FF3562', cursor: 'pointer'}} 
+				onClick={() => onDestroy(task.id)} />
 			</h3>
 			<p>{task.date}</p>
 		</div>
